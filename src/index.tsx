11fca3139fd3356d45fe926cb57805058145e548
BisiDavi <oludavidconnect@gmail.com>
2022-10-15 00:07:13 +0100

    fetchComment added

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,17 @@
-import ForgeUI, {
-  render,
-  Fragment,
-  Macro,
-  Text,
-  useProductContext,
-  useState,
-} from "@forge/ui";
-import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment } from "@forge/ui";
 
 import ModalView from "./components/ModalView";
 
-const fetchCommentsFromContent = async (contentId) => {
-  const res = await api
-    .asUser()
-    .requestConfluence(
-      route`/wiki/rest/api/content/${contentId}/child/comment`
-    );
-  const data = await res.json();
-
-  return data.results;
-};
-
 const App = () => {
-  const context = useProductContext();
-  const [comments] = useState(
-    async () => await fetchCommentsFromContent(context.contentId)
-  );
+  // const context = useProductContext();
+  // const [comments] = useState(
+  //   async () => await fetchCommentsFromContent(context.contentId)
+  // );
 
-  console.log(`Number of comments on this page: ${comments.length}`);
+  // console.log(`Number of comments on this page: ${comments.length}`);
 
   return (
     <Fragment>
-      <Text>Hello David!</Text>
-      <Text> Comments: {comments.length}</Text>
       <ModalView />
     </Fragment>
   );
