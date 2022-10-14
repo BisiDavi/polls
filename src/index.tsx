e469e701ce37e2186a2e6b615ff3ffc883355278
BisiDavi <oludavidconnect@gmail.com>
2022-10-15 00:38:57 +0100

    page updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,6 @@ import ForgeUI, { render, Fragment } from "@forge/ui";
 import ModalView from "./components/ModalView";
 
 const App = () => {
-  // const context = useProductContext();
-  // const [comments] = useState(
-  //   async () => await fetchCommentsFromContent(context.contentId)
-  // );
-
-  // console.log(`Number of comments on this page: ${comments.length}`);
-
   return (
     <Fragment>
       <ModalView />
