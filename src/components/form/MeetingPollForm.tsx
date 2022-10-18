27a0bff09b46aa7a93993e01de751085ac4e8d05
BisiDavi <oludavidconnect@gmail.com>
2022-10-18 03:38:55 +0100

    poll updated

--- src/components/form/MeetingPollForm.tsx
+++ src/components/form/MeetingPollForm.tsx
@@ -6,7 +6,7 @@ import ForgeUI, {
   TextArea,
   DatePicker,
   Button,
-  Fragment,
+  useEffect,
 } from "@forge/ui";
 import { useContentProperty } from "@forge/ui-confluence";
 
@@ -16,23 +16,14 @@ import isDateValid from "../../lib/isDateValid";
 
 export default function MeetingPollForm({ actionButton }: any) {
   const [formState, setFormState] = useState(undefined);
+  const [validDate, setValidDate] = useState(null);
   const [agenda, setAgenda] = useState(["Topic 1"]);
   const [meetingId, setMeetingId] = useContentProperty("meetingId", 0);
   const [meetingContent, setMeetingContent] = useContentProperty(
-    `meeting-poll-${meetingId}`,
+    "meetingFormData",
     ""
   );
-  const [count, setCount] = useContentProperty("count", 0);
 
-  console.log("count", count);
-
-  async function updateMeetingId() {
-    await setMeetingId(meetingId + 1);
-  }
-
-  async function updateMeetingContent(meetingData) {
-    await setMeetingContent(meetingData);
-  }
   const date = new Date();
 
   const onSubmit = async (formData) => {
@@ -46,63 +37,69 @@ export default function MeetingPollForm({ actionButton }: any) {
       date: date.toISOString();
     }
     setFormState({ ...agendaObj, ...formData });
-    updateMeetingId();
-    await setMeetingId(meetingId + 1);
-    await setMeetingContent(formState);
+    // await setMeetingId(meetingId + 1);
   };
 
-  const isMeetingDateValid = formState
-    ? isDateValid(formState.meetingDate)
-    : false;
+  // useEffect(async () => {
+  //   if (formState !== undefined && meetingContent.length === 0) {
+  //     await setMeetingContent(formState);
+  //   }
+  // }, [formState, meetingContent]);
+
+  useEffect(() => {
+    if (formState !== undefined) {
+      const dateStatus = isDateValid(formState.meetingDate);
+      console.log(
+        "dateStatus",
+        dateStatus,
+        "formState.meetingDate",
+        formState.meetingDate
+      );
+      setValidDate(dateStatus);
+    }
+  }, [formState]);
+
+  console.log("validDate", validDate);
 
   console.log("formState", formState);
   console.log("meetingId", meetingId);
-  console.log("meetingContent", meetingContent);
+  console.log("meetingFormData", meetingContent);
 
   return (
-    <Fragment>
-      <Button
-        text={`Count is ${count}`}
-        appearance="primary"
-        onClick={async () => {
-          await setCount(count + 1);
-        }}
+    <Form
+      submitButtonAppearance="primary"
+      actionButtons={actionButton}
+      onSubmit={onSubmit}
+    >
+      <Heading>Meeting Poll Form</Heading>
+      {validDate !== null && !validDate && (
+        <Button
+          text="Invalid date, meeting date must be in the future or today"
+          onClick={() => null}
+          appearance="danger"
+          icon="error"
+          iconPosition="before"
+        />
+      )}
+      <TextField
+        name="title"
+        label="Meeting Title"
+        placeholder="Enter your Meeting Title"
+        isRequired
       />
-      <Form
-        submitButtonAppearance="primary"
-        actionButtons={actionButton}
-        onSubmit={onSubmit}
-      >
-        <Heading>Meeting Poll Form</Heading>
-        {isMeetingDateValid && (
-          <Button
-            text="Invalid date, meeting date must be in the future or today"
-            onClick={() => null}
-            appearance="danger"
-            icon="error"
-            iconPosition="before"
-          />
-        )}
-        <TextField
-          name="title"
-          label="Meeting Title"
-          placeholder="Enter your Meeting Title"
-          isRequired
-        />
-        <TextField
-          name="link"
-          label="Meeting Link"
-          placeholder="zoom/google meet/any video call link"
-        />
-        <DatePicker
-          name="meetingDate"
-          placeholder="Select Date"
-          label="Pick Meeeting Date"
-          isRequired
-        />
-        <TextArea label="Meeting Description" spellCheck name="description" />
-        <PollsFieldSet type="meeting" poll={agenda} setPoll={setAgenda} />
-      </Form>
-    </Fragment>
+      <TextField
+        name="link"
+        label="Meeting Link"
+        placeholder="zoom/google meet/any video call link"
+      />
+      <DatePicker
+        name="meetingDate"
+        placeholder="Select Date"
+        label="Pick Meeeting Date"
+        isRequired
+      />
+      <TextArea label="Meeting Description" spellCheck name="description" />
+      <PollsFieldSet type="meeting" poll={agenda} setPoll={setAgenda} />
+    </Form>
   );
 }
