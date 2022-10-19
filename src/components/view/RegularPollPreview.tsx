767fc6ab64a70e5cd03cc128855380bd1d89e456
BisiDavi <oludavidconnect@gmail.com>
2022-10-19 17:24:03 +0100

    view added

---
+++ src/components/view/RegularPollPreview.tsx
@@ -0,0 +1,9 @@
+import ForgeUI, { Fragment, Text } from "@forge/ui";
+
+export default function RegularPollPreview() {
+  return (
+    <Fragment>
+      <Text>RegularPollPreview</Text>
+    </Fragment>
+  );
+}
