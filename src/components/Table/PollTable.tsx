a34e4c81f4667553ad68ecfb8dc54642975e5669
BisiDavi <oludavidconnect@gmail.com>
2022-10-21 04:32:27 +0100

    PollTable added

---
+++ src/components/Table/PollTable.tsx
@@ -0,0 +1,51 @@
+import usePublish from "@/hooks/usePublish";
+import ForgeUI, {
+  Text,
+  useState,
+  useEffect,
+  Table,
+  Head,
+  Cell,
+} from "@forge/ui";
+
+export default function PollTable() {
+  const [savedPolls, setSavedPolls] = useState(null);
+  const { getSavedPolls } = usePublish();
+  const polls = [];
+
+  useEffect(async () => {
+    if (savedPolls === null) {
+      await getSavedPolls().then((response) => {
+        let pollData = {};
+        response.results.map((item: any) => {
+          pollData = {
+            value: JSON.parse(item?.value),
+            key: item.key,
+          };
+          polls.push(pollData);
+        });
+        setSavedPolls(polls);
+      });
+    }
+  }, []);
+
+  const head = [
+    { key: "id", text: "S/N" },
+    { key: "Title", text: "Title" },
+    { key: "Author", text: "Author" },
+    { key: "Date", text: "Date Created" },
+    { key: "PollType", text: "Poll Type" },
+    { key: "PollStatus", text: "Poll Status" },
+  ];
+  return (
+    <Table>
+      <Head>
+        {head.map((item) => (
+          <Cell key={item.key}>
+            <Text>{item.text}</Text>
+          </Cell>
+        ))}
+      </Head>
+    </Table>
+  );
+}
