76d6a4ef5e5112725aa5c2f3d6ab82bc2b112e49
BisiDavi <oludavidconnect@gmail.com>
2022-10-25 09:39:04 +0100

    poll modal view updated

--- src/components/modal/PollModal.tsx
+++ src/components/modal/PollModal.tsx
@@ -23,3 +23,6 @@ export default function PollModal({ setPollModal, data }) {
     </ModalDialog>
   );
 }
+
+
+//  import CheckIcon from '@atlaskit/icon/glyph/check'
--- src/components/view/MakePollModalView.tsx
+++ src/components/view/MakePollModalView.tsx
@@ -1,23 +1,58 @@
-import ForgeUI, { Fragment, Text, Em } from "@forge/ui";
+import ForgeUI, {
+  Fragment,
+  Text,
+  Em,
+  Button,
+  Heading,
+  useState,
+} from "@forge/ui";
 
 import { formatPollTopic } from "../../lib/getAgendaName";
 
 export default function MakePollModalView({ data }) {
   const formatPollType = data.type === "meetingPoll" ? "topic" : "poll";
   const topics = data ? formatPollTopic(data, formatPollType) : null;
+  const [poll, makePoll] = useState(null);
+
+  //   const buttonAppearance: any = (item: string): any =>
+  //     poll === item ? "primary" : "subtle";
+
+  //   const buttonIcon = (item: string): any =>
+  //     poll === item ? "check-circle" : "presence-active";
+
+  const disableButton =
+    poll === null ? false : typeof poll === "string" && true;
 
   return (
     <Fragment>
+      <Heading>{data.title}</Heading>
       <Text>
-        Poll for
-        {data.title}
+        <Em>Note: Click on the Button to Vote</Em>
       </Text>
       {topics &&
-        topics?.map((item, index) => (
-          <Text key={item}>
-            {index + 1}. <Em>{item}</Em>
-          </Text>
-        ))}
+        topics?.map((item, index) => {
+          const buttonAppearance: any = poll === item ? "primary" : "subtle";
+
+          const buttonIcon = poll === item ? "check-circle" : "presence-active";
+
+          return (
+            <Button
+              key={index}
+              icon={buttonIcon}
+              iconPosition="before"
+              appearance={buttonAppearance}
+              text={item}
+              onClick={() => makePoll(item)}
+              disabled={disableButton}
+            />
+          );
+        })}
+      <Button
+        text="Reset"
+        icon="error"
+        appearance="danger"
+        onClick={() => makePoll(null)}
+      />
     </Fragment>
   );
 }
