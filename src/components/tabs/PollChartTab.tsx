2303e61659f77aeb6f4eb07b6dae1afcf21b13bb
BisiDavi <oludavidconnect@gmail.com>
2022-10-26 05:09:03 +0100

    chart updated

--- src/components/tabs/PollChartTab.tsx
+++ src/components/tabs/PollChartTab.tsx
@@ -5,7 +5,7 @@ import getChartData from "../../lib/getChartData";
 export default function PollChartTab({ data, poll }) {
   const chartData = getChartData(data, poll);
   console.log("PollChartTab-chartData", chartData);
-  console.log("PollChartTab-chartData[dataset]", chartData.data.datasets);
+  console.log(`PollChartTab-chartData[dataset]-${poll}`, chartData.data.datasets);
   const chartLink = `https://quickchart.io/chart?c=${chartData}`;
   console.log("chartLink", chartLink);
   return (
--- src/lib/getChartData.ts
+++ src/lib/getChartData.ts
@@ -1,9 +1,19 @@
 import { getPollChartDataArray } from "./formatVote";
 import generateRandomColor from "./generateRandomColor";
 
-export default function getChartpollData(data, type) {
-  const { pollOptions, title, pollData } = data;
-  const chartData = getPollChartDataArray(pollOptions, pollData);
+function pieChart(pollOptions, chartData) {
+  const data = [];
+  const backgroundColor = [];
+  chartData.map((item) => data.push(item.count));
+  pollOptions.map((_) => {
+    const randomColor = generateRandomColor();
+    backgroundColor.push(randomColor);
+  });
+  const pieChartDataset = [{ data, backgroundColor, label: "" }];
+  return pieChartDataset;
+}
+
+function barChart(pollOptions, chartData) {
   const barChartDataset = [];
   pollOptions.map((item) => {
     const dataCount = chartData.filter((d) => d.vote === item)[0];
@@ -13,13 +23,28 @@ export default function getChartpollData(data, type) {
       backgroundColor: generateRandomColor(),
     });
   });
+}
+
+export default function getChartpollData(data, type) {
+  const { pollOptions, title, pollData } = data;
+  const chartData = getPollChartDataArray(pollOptions, pollData);
+  const barChartDataset = barChart(pollOptions, chartData);
+  const pieChartDataset = pieChart(pollOptions, chartData);
+  const polarAreaLegend = {
+    legend: {
+      position: "right",
+    },
+  };
+  const legend = type === "polar-area" ? polarAreaLegend : "";
+
   return {
     type,
     data: {
       labels: pollOptions,
-      datasets: barChartDataset,
+      datasets: type === "bar" ? barChartDataset : pieChartDataset,
     },
     options: {
+      ...legend,
       title: {
         display: true,
         text: title,
