cd992c4b6829387431bb6e850426abfba4e843ed
BisiDavi <oludavidconnect@gmail.com>
2022-10-29 20:43:59 +0100

    notify team updated

--- src/components/view/NotifyTeam.tsx
+++ src/components/view/NotifyTeam.tsx
@@ -2,6 +2,7 @@ import ForgeUI, {
   Button,
   Form,
   Fragment,
+  SectionMessage,
   Strong,
   Text,
   TextArea,
@@ -9,11 +10,17 @@ import ForgeUI, {
   useState,
 } from "@forge/ui";
 import { fetch } from "@forge/api";
+import { formatPollAgenda } from "src/lib/getAgendaName";
 
 export default function NotifyTeam({ data }) {
   const [notify, setNotify] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [formState, setFormState] = useState(undefined);
+
+  function notifyHandler() {
+    setNotify(true);
+    setSubmitted(false);
+  }
 
   async function onSubmit(formData) {
     formData: {
@@ -23,40 +30,61 @@ export default function NotifyTeam({ data }) {
   }
 
   console.log("formState", formState);
+  const pollType = data.type === "meetingPoll" ? "Meeting" : "Poll";
 
   useEffect(async () => {
     if (formState !== undefined && !submitted) {
+      const messageLink =
+        typeof data.link === "string"
+          ? `Meeting link:${data.link}`
+          : `Start Url (Host):${data.link.start_url}\nJoin Url(Invite):${data.link.join_url}\nMeeting Password:${data.link.password}`;
+      let agendaString = "";
+      const formatPollType = data.type === "meetingPoll" ? "agenda" : "poll";
+      const agendasArray = data ? formatPollAgenda(data, formatPollType) : null;
+      agendasArray &&
+        agendasArray.map((item) => {
+          agendaString += `-${item}\n`;
+        });
+
+      const meetingData = `Title:${data.title}\nDescription:${data.description}\nTime:${data.time}\nDuration:${data.duration}\n${messageLink}\nMeeting Date:${data.meetingDate}\n\n\n\n\nAgendas to be discussed:\n${agendaString}}`;
+
       await fetch("https://confluence-api.vercel.app/api/gmail/mail/send", {
         method: "POST",
         body: JSON.stringify({
           title: data.title,
           receipent: formState.teamEmail,
-          message: "",
+          message: meetingData,
         }),
-      }).then((response) => {
+      }).then(() => {
         setSubmitted(true);
         setNotify(false);
+        setFormState(undefined);
       });
-      const messageData = {};
     }
   }, [formState]);
 
   return (
     <Fragment>
+      {submitted && (
+        <SectionMessage title="Team Members Notified" appearance="confirmation">
+          <Text>Email Notification Sent Successfully</Text>
+        </SectionMessage>
+      )}
       <Button
         text="Approve & Notify Team"
         icon="check-circle-outline"
         iconPosition="before"
         appearance="primary"
-        onClick={() => setNotify(true)}
+        onClick={notifyHandler}
       />
       {notify && (
         <Fragment>
           <Text>
-            Enter Team member emails, ensure to use <Strong>comma</Strong>for
-            multiple email
+            Send team member notification email about the {pollType}, enter team
+            member emails, ensure to use <Strong>comma</Strong>for multiple
+            emails
           </Text>
-          <Form submitButtonAppearance="primary" onSubmit={() => null}>
+          <Form submitButtonAppearance="primary" onSubmit={onSubmit}>
             <TextArea
               name="teamEmail"
               label="Enter team emails"
